7a58f6a71ba17ceed5ccaccfc3db45f74da13429
bozr456325 <ruslankakaev5@gmail.com>
2026-01-30 15:09:48 +0300

    main

---
+++ config.js
@@ -0,0 +1,4 @@
+// Конфиг для хостинга (GitHub Pages и других статических хостингов).
+// Укажи URL твоего бота (где запущен bot.py), чтобы работали API: поиск пользователей, Fragment, Steam и т.д.
+// Пример: 'https://api.jetstoreapp.ru' или 'https://твой-туннель.ngrok.io'
+window.JET_API_BASE = '';
